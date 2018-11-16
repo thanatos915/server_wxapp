01c751d7e3e9ed28463480a15a7e4f3305f03431
thanatos <thanatos915@163.com>
2018-11-16 11:15:21 +0800

    购买记录

--- modules/api/controllers/ShopController.php
+++ modules/api/controllers/ShopController.php
@@ -6,14 +6,17 @@
 namespace app\modules\api\controllers;
 
 
+use app\hejiang\ApiResponse;
 use app\hejiang\BaseApiResponse;
 use app\models\Option;
 use app\models\Order;
 use app\models\Setting;
 use app\models\Shop;
 use app\models\User;
+use app\models\UserShareMoney;
 use Yii;
 use app\modules\api\behaviors\LoginBehavior;
+use yii\data\Pagination;
 
 class ShopController extends Controller
 {
@@ -79,6 +82,26 @@ class ShopController extends Controller
             'msg' => 'success',
             'data' => $data,
         ]);
+    }
+
+    public function actionAccountLog()
+    {
+
+        $params = Yii::$app->request->get();
+
+        $query = UserShareMoney::find()->alias('us')->where(['us.user_id' => Yii::$app->user->id, 'us.is_delete' => 0, 'us.store_id' => $this->store->id])->leftJoin(['o' => Order::tableName(), 'o.id' => 'us.order_id'])->select(['us.money', 'us.addtime', 'o.pay_price', 'o.name']);
+
+        $count = $query->count();
+
+        $pagination = new Pagination(['totalCount' => $count, 'pageSize' => $params['limit'], 'page' => $params['page'] - 1]);
+
+        $list = $query->limit($pagination->limit)->offset($pagination->offset)->orderBy('o.addtime DESC')->asArray()->all();
+        $data = [
+            'row_count' => $count,
+            'page_count' => $pagination->pageCount,
+            'list' => $list,
+        ];
+        return new ApiResponse(0, 'success', $data);
     }
 
 }
